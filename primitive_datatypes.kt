791ea74fba9bad88ed28b181f858ee9da98b4334
Navya p p <ammunavya07@gmail.com>
2025-09-19 11:43:17 +0530

    Create primitive_datatypes.kt
    Primitive datatypes in Kotlin

---
+++ primitive_datatypes.kt
@@ -0,0 +1,25 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+   	val x1: Int = 5 // integer datatype
+    println(x1)
+    println(5+7)
+    
+    val x2: Long = 1000000L // Long datatype
+    println(x2)
+    
+    val x3: Float = 3.14f // float datatype
+    println(x3)
+    
+    val x4: Double = 925637.0004627863278 // double datatype
+    println(x4)
+    
+    val y1: Boolean = true // boolean datatype
+    val y2: Boolean = false // boolean datatype
+    println("" + y2 + "," + y1)
+    
+    val z1: Char = 'k' // char datatype
+    println(z1)
+}
