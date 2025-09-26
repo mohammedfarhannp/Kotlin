272c7f671bc173ed7801fcb1213f9d57485f1306
Navya p p <ammunavya07@gmail.com>
2025-09-26 12:25:30 +0530

    Create basic-for-loops.kt

---
+++ Looping-Statements/basic-for-loops.kt
@@ -0,0 +1,15 @@
+fun main() {
+	// basic for loop 1 to 10
+    for(i in 1..10)
+    {
+        println(i)
+    }
+    
+    println()
+    
+    // basic for loop 1 to 9 using 'until'
+    for(i in 1 until 10)
+    {
+        println(i)
+    }
+}
